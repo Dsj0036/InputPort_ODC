b2889aa00df57224d9ffe41dee7386ff41486e76
D.s.j <97750772+Dsj0036@users.noreply.github.com>
2024-03-08 16:21:58 -0300

    Sample test finish.

--- DreidelClient/InputConverter.h
+++ DreidelClient/InputConverter.h
@@ -2,24 +2,49 @@
 #include "PS3/System.h"
 #include "PS3/PadHelper.h"
 #include "cell/pad/libpad_dbg.h"
-namespace ICNV
+#include "PS3/Dialog.h"
+namespace IConverter
 {
 	bool con[2]{false ,false};
 	bool okn = false;
 	bool ok = false;
 	point interval(0,10);
 	point interval_of(0,5);
-	bool 
+	bool Registered = false;
+	uint HANDLE = -1;
 	void WhenDisconnected()
 	{
-
+		ok = false;
+		okn = false;
+	}
+	void ForceDisconnect() {
+		con[0] = false;
+		con[1] = false;
+		ok = false;
+		okn = false;
+	}
+	void Work() {
+		CellKbData kbdata;
+		CellMouseData msdata;
+		uint e = Pad::GetKeyboardData(0, &kbdata);
+		uint ee = Pad::GetMouseData(0, &msdata);
+		if ((e + ee) == 0 && Registered) {
+			//CellPadData data;
+			//kbdata.keycode
+			if (kbdata.keycode[0] == (CELL_KEYC_SPACE | CELL_KB_RAWDAT)) {
+				diag::ShowOk("U pressed SPACE.");
+			}
+			//data.len = 128;
+		}
+		else
+		{
+			WhenDisconnected();
+		}
 	}
 	void Check() {
-		bool a = con[0];
-		bool b = con[1];
+		bool a = (con[0] = Pad::GetConnectedMouseCount()	>= 1);
+		bool b = (con[1] = Pad::GetConnectedKeyboardCount() >= 1);
 		if (!(a || b)) {
-			a = Pad::GetConnectedMouseCount() >= 1;
-			b = Pad::GetConnectedKeyboardCount() >= 1;
 			// 1: mouse | 2: keyboard
 			int trigger = a ? 1 : b ? 2 : 0;
 
@@ -33,12 +58,26 @@ namespace ICNV
 			}
 
 		}
-		
+		else
+		{
+
+			bool wasConnected = okn == true;
+
+			ok = a && b;
+
+			okn = wasConnected && ok;
+			
+			if (ok == false && wasConnected)
+			{
+				WhenDisconnected();
+				okn = false;
+			}
+		}
 		
 	}
 	void Tick()
 	{
-		bool readable = ok && okn;
+		bool readable = ok && okn && Registered;
 		if ((++interval.x) > interval.y)
 		{	
 			// Normal tick
@@ -48,18 +87,35 @@ namespace ICNV
 				// big tick
 				interval_of.x = 0;
 				Pad::Clear(0);
+
+				if (Pad::IsDS3ConnectedPort(0)) {
+					ForceDisconnect();
+				}
 			}
 		}
 		if (readable) {
+			Work();
+		}
+	}
+	void Register() {
+
+		//uint r = cellPadDbgLddRegisterController(CELL_PAD_CAPABILITY_PS3_CONFORMITY | CELL_PAD_CAPABILITY_HP_ANALOG_STICK |	CELL_PAD_CAPABILITY_ACTUATOR | CELL_PAD_CAPABILITY_PRESS_MODE);
+		Registered = true;//!CELL_STATUS_IS_FAILURE(r);
+		//HANDLE = r;
+		int kbe = cellKbInit(2);
+		debug::out::writeif(kbe != 0, "CellKbInit Failed");
+		Registered = Registered && kbe == 0;
+		if (Registered) {
+			cellKbSetLEDStatus(0, CellKbLedMode::CELL_KB_LED_MODE_AUTO1);
+			cellKbSetCodeType(0, CELL_KB_CODETYPE_RAW);
+			cellKbSetReadMode(0, 1); // Readmode: PACKET
 			
 		}
 	}
 	void Init() {
 		interval.x = 0;
 		interval.y = 10;
-		cellPadDbgLddRegisterController
-		(CELL_PAD_CAPABILITY_PS3_CONFORMITY | CELL_PAD_CAPABILITY_HP_ANALOG_STICK |
-			CELL_PAD_CAPABILITY_ACTUATOR | CELL_PAD_CAPABILITY_PRESS_MODE);
-
+		interval.y = 10;
+		Register();
 	}
 }
---
+++ DreidelClient/PS3/Dialog.h
@@ -0,0 +1,107 @@
+#include <sysutil/sysutil_msgdialog.h>
+#include <sysutil/sysutil_oskdialog.h>
+#include <sysutil/sysutil_oskdialog_ext.h>
+
+#include <sys/prx.h>
+#include <cellstatus.h>
+#include <wchar.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <sys/ppu_thread.h>
+#include <string.h>
+#include <sys/sys_time.h>
+#include <sys/time_util.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <sys/process.h>
+#include <sys/memory.h>
+#include <sys/timer.h>
+#include <sys/return_code.h>
+#include <stddef.h>
+#include <math.h>
+#include <stdarg.h>
+#include <cellstatus.h>
+#include <typeinfo>
+#include <vector>
+#include <pthread.h>
+#include <locale.h>
+#include <cell/error.h>
+#include <sys/paths.h> 
+#include <sys/fs.h> 
+#include <sys/fs_external.h> 
+#include <cell/cell_fs.h>
+#include <cell/fs/cell_fs_file_api.h>
+#include <time.h>
+#include <sys/return_code.h>
+#include <sysutil/sysutil_oskdialog.h>
+#include <sysutil/sysutil_msgdialog.h>
+#include "Trace.h"
+#pragma once
+namespace diag
+{
+	void Show(char* message, float delayMs = 10000)
+	{
+		cellMsgDialogOpen(
+			CELL_MSGDIALOG_TYPE_BUTTON_TYPE_OK |
+			CELL_MSGDIALOG_TYPE_BG_VISIBLE
+			, message, NULL, NULL, NULL);
+		cellMsgDialogClose(delayMs);
+	}
+	void RequestReportProgress(unsigned int index, int value) {
+		cellMsgDialogProgressBarInc(index, value);
+	}
+	void RequestSetMessage(int index, char* msg) {
+		cellMsgDialogProgressBarSetMsg(index, msg);
+	}
+	void ShowProgressC(char* message, char* bottomMsg, int delayMs = 3500, int initialValue = 50)
+	{
+		unsigned int type =
+			CELL_MSGDIALOG_TYPE_SE_TYPE_NORMAL | CELL_MSGDIALOG_TYPE_DEFAULT_CURSOR_NONE | CELL_MSGDIALOG_TYPE_BUTTON_TYPE_NONE | CELL_MSGDIALOG_TYPE_BG_VISIBLE | CELL_MSGDIALOG_TYPE_PROGRESSBAR_SINGLE;
+		cellMsgDialogOpen2(type, message, NULL, NULL, NULL);
+		cellMsgDialogProgressBarSetMsg(CELL_MSGDIALOG_PROGRESSBAR_INDEX_SINGLE, bottomMsg);
+		RequestReportProgress(CELL_MSGDIALOG_PROGRESSBAR_INDEX_SINGLE, initialValue);
+		cellMsgDialogClose(delayMs == 0 ? 3500 : delayMs);
+	}
+	void ShowProgress(char* message, char* bottomMsg, int initialValue = 50)
+	{
+		unsigned int type =
+			CELL_MSGDIALOG_TYPE_SE_TYPE_NORMAL | CELL_MSGDIALOG_TYPE_DEFAULT_CURSOR_NONE | CELL_MSGDIALOG_TYPE_BUTTON_TYPE_NONE | CELL_MSGDIALOG_TYPE_BG_VISIBLE | CELL_MSGDIALOG_TYPE_PROGRESSBAR_SINGLE | CELL_MSGDIALOG_TYPE_DISABLE_CANCEL_ON;
+		cellMsgDialogOpen2(type, message, NULL, NULL, NULL);
+		cellMsgDialogProgressBarSetMsg(CELL_MSGDIALOG_PROGRESSBAR_INDEX_SINGLE, bottomMsg);
+		RequestReportProgress(CELL_MSGDIALOG_PROGRESSBAR_INDEX_SINGLE, initialValue);
+	}
+	void Show(char* message, int initialProgress, int delayMs = 10000)
+	{
+		cellMsgDialogOpen(
+			CELL_MSGDIALOG_TYPE_PROGRESSBAR_SINGLE |
+			CELL_MSGDIALOG_TYPE_BG_VISIBLE |
+			CELL_MSGDIALOG_TYPE_DISABLE_CANCEL_OFF,
+			message, NULL, NULL, NULL);
+		cellMsgDialogProgressBarInc(initialProgress, 1);
+		cellMsgDialogClose(delayMs);
+	}
+	void ShowOk(char* message) {
+		unsigned int type =
+			CELL_MSGDIALOG_TYPE_BG_VISIBLE | CELL_MSGDIALOG_TYPE_BUTTON_TYPE_OK | CELL_MSGDIALOG_TYPE_DEFAULT_CURSOR_OK | CELL_MSGDIALOG_TYPE_DISABLE_CANCEL_ON;
+		cellMsgDialogOpen(type, message, NULL, NULL, NULL);
+	}
+	void Show(char* message, int progress, int delta, char* bottomText)
+	{
+		cellMsgDialogOpen(CELL_MSGDIALOG_TYPE_PROGRESSBAR_SINGLE |
+			CELL_MSGDIALOG_TYPE_BG_VISIBLE |
+			CELL_MSGDIALOG_TYPE_DISABLE_CANCEL_OFF, message, NULL, NULL, NULL);
+
+		cellMsgDialogProgressBarInc(progress, delta);
+		cellMsgDialogProgressBarSetMsg(progress, bottomText);
+	}
+
+	void SetMessageActive(int index, char* bottomText)
+	{
+		cellMsgDialogProgressBarSetMsg(index, bottomText);
+	}
+	/*
+
+	*/
+#pragma once
+}
